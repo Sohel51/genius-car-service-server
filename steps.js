9ece510b2e26618110f77939ae239000c8373802
Sohel51 <sohelcutyy@gmail.com>
2022-11-21 22:57:09 +0600

    created genius car server

---
+++ steps.js
@@ -0,0 +1,17 @@
+/**
+ * ----------------------Creating a Server Site----------------------
+ * 1. Open cmd
+ * 2. mkdir project-name
+ * 3. open the project in vs (code .)
+ * 4. npm init -y
+ * 5. npm i express cors mongodb dotenv
+ *      express- for using node version
+ *      cors- for communicating front-end & back-end
+ *      mongodb- for database
+ *      dotenv- for special purpose
+ * 6.
+ * 
+ * 
+ * 
+ * 
+ */
